bf160499bbc1fac2fcdd2c3f3a14db33209c7243
Michael-Kolpakov <93143249+Mikhail-Kolpakov@users.noreply.github.com>
2024-12-26 00:28:28 +0200

    Added controllers

--- filmio-api.BLL/DTO/Film/Create/FilmCreateRequestRequestDto.cs
+++
@@ -1,5 +0,0 @@
-namespace filmio_api.BLL.DTO.Film.Create;
-
-public class FilmCreateRequestRequestDto : FilmCreateUpdateRequestDto
-{
-}
--- filmio-api.WebApi/Program.cs
+++ filmio-api.WebApi/Program.cs
@@ -1,9 +1,12 @@
 using filmio_api.Extensions;
 using filmio_api.Middlewares;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Host.UseSerilog((context, services, loggerConfiguration) =>
+    loggerConfiguration.ReadFrom.Configuration(context.Configuration).ReadFrom.Services(services));
+
 builder.Services.AddApplicationServices(builder.Configuration, builder.Environment);
 
 var app = builder.Build();
