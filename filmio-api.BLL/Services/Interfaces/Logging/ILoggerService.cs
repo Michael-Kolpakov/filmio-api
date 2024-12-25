a065bf1315ad2e13103605f0494e3c219e708c8a
Michael-Kolpakov <93143249+Mikhail-Kolpakov@users.noreply.github.com>
2024-12-25 16:57:21 +0200

    Implemented logger service

---
+++ filmio-api.BLL/Services/Interfaces/Logging/ILoggerService.cs
@@ -0,0 +1,12 @@
+namespace filmio_api.BLL.Services.Interfaces.Logging;
+
+public interface ILoggerService
+{
+    void LogInformation(string message);
+
+    void LogWarning(string message);
+
+    void LogDebug(string message);
+
+    void LogError(object? request, string errorMessage);
+}
