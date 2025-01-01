83ad632806f26683a9a2b4c1b12d32d04be40b5b
Michael-Kolpakov <93143249+Mikhail-Kolpakov@users.noreply.github.com>
2025-01-01 15:57:36 +0200

    Made some dto validation changes + new initial data

--- Filmio.BLL/DTO/Film/FilmCreateUpdateRequestDto.cs
+++ Filmio.BLL/DTO/Film/FilmCreateUpdateRequestDto.cs
@@ -6,11 +6,11 @@ namespace Filmio.BLL.DTO.Film;
 public class FilmCreateUpdateRequestDto
 {
     [Required(ErrorMessage = "{0} can't be empty or null")]
-    [StringLength(40, ErrorMessage = "{0} should be less than {1} characters")]
+    [StringLength(50, ErrorMessage = "{0} should be less than {1} characters")]
     public string? Title { get; set; }
 
     [Required(ErrorMessage = "{0} can't be empty or null")]
-    [StringLength(40, ErrorMessage = "{0} should be less than {1} characters")]
+    [StringLength(60, ErrorMessage = "{0} should be less than {1} characters")]
     [RegularExpression(@"^[a-zA-Z| ]*$", ErrorMessage = "{0} can only contain latin letters, spaces and the '|' character")]
     public string? Genre { get; set; }
 
@@ -24,7 +24,7 @@ public class FilmCreateUpdateRequestDto
     [ReleaseDate]
     public DateTime ReleaseDate { get; set; }
 
-    [Range(0, 10, ErrorMessage = "{0} should be between {1} and {2}")]
+    [Range(1, 5, ErrorMessage = "{0} should be between {1} and {2}")]
     public float? Rating { get; set; }
 
     [StringLength(500, ErrorMessage = "{0} should be less than {1} characters")]
