2752a51f2956ec0a0d533447fba8259267b15b35
Michael-Kolpakov <93143249+Mikhail-Kolpakov@users.noreply.github.com>
2025-01-03 18:44:40 +0200

    Added password min length validation

--- Filmio.BLL/DTO/Authentication/Login/LoginRequestDto.cs
+++ Filmio.BLL/DTO/Authentication/Login/LoginRequestDto.cs
@@ -9,6 +9,7 @@ public class LoginRequestDto
     public string? Email { get; set; }
 
     [Required(ErrorMessage = "{0} can't be empty or null")]
+    [MinLength(6, ErrorMessage = "{0} must be at least {1} characters long")]
     [RegularExpression(@"^[a-zA-Z0-9]*$", ErrorMessage = "{0} can only contain numbers and latin letters")]
     public string? Password { get; set; }
 }
